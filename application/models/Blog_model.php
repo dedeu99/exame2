83dd1cad9657ba45eb3b517bf3a130d9f650997d
dedeu99 <andre.calixto.martins@gmail.com>
2019-01-17 15:53:09 +0000

    asd

--- application/models/Blog_model.php
+++ application/models/Blog_model.php
@@ -129,7 +129,7 @@
 
 			
 			$query = $this->db->query("SELECT u.name, m.id, m.user_id, m.content, m.created_at, m.updated_at, m.likes,m.public from users as u inner join (microposts as m) on (u.id=m.user_id) where public='1' OR (public='0' AND u.id IN (
-				SELECT friend_user_id FROM friends WHERE user_id='".$_SESSION['id']."'
+				SELECT user_id FROM friends WHERE friend_user_id='".$_SESSION['id']."'
 
 
 		)) order by m.updated_at desc");
